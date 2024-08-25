05b4c3a62dc1522c967507fc9400b615c1c64f37
gautham <desertncamel@gmail.com>
2024-08-25 10:32:52 +0530

    yummy logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,57 +22,48 @@ function Navbar() {
         isScrolled ? "shadow-lg" : ""
       } bg-white`}
     >
-      <div className="max-w-3xl lg:max-w-7xl mx-auto flex items-center justify-between h-20">
+        <div className="max-w-3xl lg:max-w-7xl mx-auto flex items-center justify-between h-20">
         
-        {/* Left Section - Logo */}
-        <div className="flex items-center">
-          <img
-            className="h-8 w-8"
-            src="https://cdn-icons-png.flaticon.com/512/2111/2111425.png"
-            alt="Logo"
-          />
+            {/* Left Section - Logo */}
+            <div className="flex items-center">
+                <img
+                    className="h-8 w-8"
+                    src="https://cdn-icons-png.flaticon.com/512/2111/2111425.png"
+                    alt="Logo"
+                />
 
-        <div className="title">
-        <span className="highlight">
-            <a href=""><span className="text ml-3 text-xl font-semibold text-amber-600 hover:text-amber-400 hover:font-bold transition-all duration-200 ease-in-out">Avalon</span></a>
-        </span>
+                <div className="title ml-5 hover:scale-110 transition-all duration-200 ease-in-out">
+                    <span className="highlight">
+                        <span className="text text-xl mx-2 mb-1 font-semibold text-amber-600 hover:text-amber-700 cursor-pointer">Avalon</span>
+                    </span>
+                </div>
+
+            </div>
+
+            {/* Center Section - Links */}
+            <div className="flex space-x-8">
+                <a href="#" className="text-gray-900 hover:text-gray-600 hover:font-bold transition-all duration-200 ease-in-out">
+                    Today
+                </a>
+                <a href="#" className="text-gray-900 hover:text-gray-600 hover:font-bold transition-all duration-200 ease-in-out">
+                    Explore
+                </a>
+                <a href="#" className="text-gray-900 hover:text-gray-600 hover:font-bold transition-all duration-200 ease-in-out">
+                    About
+                </a>
+            </div>
+
+            {/* Login / SignUp */}
+            <div className="flex space-x-4">
+                <button className="px-4 py-2 items-center bg-amber-500 text-white rounded-full hover:bg-amber-300 transition-all duration-200 ease-in-out">
+                    Log in
+                </button>
+                <button className="px-4 py-2 bg-gray-200 text-gray-900 rounded-full hover:bg-gray-300 transition-all duration-200 ease-in-out">
+                    Sign up
+                </button>
+            </div>
+
         </div>
-
-
-
-        
-        
-            
-        </div>
-
-
-
-        </div>
-
-        {/* Center Section - Links */}
-        <div className="flex space-x-8">
-          <a href="#" className="text-gray-900 hover:text-gray-600 hover:font-bold transition-all duration-200 ease-in-out">
-            Today
-          </a>
-          <a href="#" className="text-gray-900 hover:text-gray-600 hover:font-bold transition-all duration-200 ease-in-out">
-            Explore
-          </a>
-          <a href="#" className="text-gray-900 hover:text-gray-600 hover:font-bold transition-all duration-200 ease-in-out">
-            About
-          </a>
-        </div>
-
-        {/* Login / SignUp */}
-        <div className="flex space-x-4">
-            <button className="px-4 py-2 items-center bg-amber-500 text-white rounded-full hover:bg-amber-300 transition-all duration-200 ease-in-out">
-                Log in
-            </button>
-            <button className="px-4 py-2 bg-gray-200 text-gray-900 rounded-full hover:bg-gray-300 transition-all duration-200 ease-in-out">
-                Sign up
-            </button>
-        </div>
-
-      </div>
     </nav>
   );
 }
