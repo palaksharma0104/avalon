da93b65773e023e06d3fae62acc1c1e4d1faba49
gautham <desertncamel@gmail.com>
2024-10-25 10:59:48 +0530

    ignore

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,23 @@
-const express = require("express");
+import express from "express";
+import connectDB from "./db/db.js";
+import authroutes from "./routes/auth.js";
+import bodyParser from "body-parser";
+// import auth from './middlewares/checktoken.js';
+
+import cors from "cors";
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.use(cors({ origin: "*" }));
+
+app.use("/api/auth", authroutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
