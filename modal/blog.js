ab68e554c6bb47259c9a4e23b87b1cb1998f7638
gautham <desertncamel@gmail.com>
2024-11-23 12:23:14 +0530

    save

--- modal/blog.js
+++ modal/blog.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const blogSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
-  author: { type: String, ref: "User" },
+  // author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  author: { type: String },
   createdAt: { type: Date, default: Date.now },
 });
 
--- routes/blogs.js
+++ routes/blogs.js
@@ -4,11 +4,35 @@ import Blog from "../modal/blog.js";
 
 router.get("/blogs", async (req, res) => {
   try {
-    const blogs = await Blog.find().populate("author", "username"); // Populate author username
+    const blogs = await Blog.find().sort({ createdAt: -1 });
+    // const blogs = await Blog.find()
+    //   .populate("author", "username name")
+    //   .sort({ createdAt: -1 });
     res.status(200).json(blogs);
   } catch (error) {
     console.error("Error fetching blogs:", error);
     res.status(500).json({ message: "Error fetching blogs" });
+  }
+});
+
+router.post("/saveblog", async (req, res) => {
+  const { title, content, author } = req.body;
+  // author should be email. and then find a user from the other one w the email, then get the id and then replace author with that id.
+  if (!title || !content) {
+    return res.status(400).json({ message: "Title and content are required" });
+  }
+
+  try {
+    const blog = new Blog({
+      title,
+      content,
+      author,
+    });
+    await blog.save();
+    res.status(201).json({ message: "Blog created successfully", blog });
+  } catch (error) {
+    console.error("Error creating blog:", error);
+    res.status(500).json({ message: "Error creating blog" });
   }
 });
 
